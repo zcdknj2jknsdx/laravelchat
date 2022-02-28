665242c73c8f3d23ab19b9e63aae6d74f28381d2
unknown <chuvantinh999@gmail.com>
2022-03-01 00:37:50 +0700

    chat

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Auth::routes();
+
+Route::get('/', 'App\Http\Controllers\ChatsController@index');
+Route::get('messages', 'App\Http\Controllers\ChatsController@fetchMessages');
+Route::post('messages', 'App\Http\Controllers\ChatsController@sendMessage');
